632b86bbdd07b6e0f399e51508dc8b943d916ee7
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2025-11-17 20:39:54 +0530

    Add basic variable type demonstrations in program.py
    Demonstrate variable types in Python including int, float, str, bool, and complex.

---
+++ Basic_programs/program.py
@@ -0,0 +1,18 @@
+num1=10
+print(num1)
+print(type(num1))
+num2=10.8
+print(num2)
+print(type(num2))
+num3="hello python"
+print(num3)
+print(type(num3))
+bool1=True
+bool2=False
+print(bool1)
+print(bool2)
+print(type(bool1))
+print(type(bool2))
+print()
+c=3+1j
+print(type(c))
