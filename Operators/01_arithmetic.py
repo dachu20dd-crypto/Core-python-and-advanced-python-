ef82bf0abbc46c1eadad0c859d4be5d8feaec62c
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2025-11-17 21:00:27 +0530

    Add basic arithmetic operations in 01_arithmetic.py

---
+++ Operators/01_arithmetic.py
@@ -0,0 +1,19 @@
+a=15
+b=10
+print("a value :",a)
+print("b value :",b)
+
+c=a+b
+print("addition:",c)
+
+d=a-b
+print("subtraction :",d)
+
+e=a/b
+print("division:",e)
+
+f=a*b
+print("multiplication:",f)
+
+g=a%b
+print("modulus:",g)
