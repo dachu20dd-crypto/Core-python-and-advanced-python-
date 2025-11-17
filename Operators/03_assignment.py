2ea0720e1ce8a16c927315c767f9205232894709
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2025-11-17 21:17:58 +0530

    Add assignment operators example in 03_assignment.py
    Demonstrate various assignment operators in Python.

---
+++ Operators/03_assignment.py
@@ -0,0 +1,18 @@
+a=10
+b=20
+print("A value :",a)
+
+a+=b#a=a+b
+print("A value :",a)
+
+a-=b#a=a-b
+print("A value :",a)
+
+a*=b#a=a*b
+print("A value :",a)
+
+a/=b#a=a/b
+print("A value :",a)
+
+a%=b#a=a%b
+print("A value :",a)
