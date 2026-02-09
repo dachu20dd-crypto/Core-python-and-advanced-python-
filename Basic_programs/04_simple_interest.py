1cfe6bf11255ebd4d0640c48ec275a54ece2876f
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2026-02-09 20:51:45 +0530

    Add simple interest calculation program

---
+++ Basic_programs/04_simple_interest.py
@@ -0,0 +1,6 @@
+p = int(input("enter the principal amount: "))
+t = int(input("enter the time period: "))
+r = int(input("enter the rate of interest: "))
+
+si = (p * t * r) / 100
+print("the simple interest:", si)
