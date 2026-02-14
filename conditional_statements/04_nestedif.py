5949b48a15c37e791ff4bfa1918e052231e01ac3
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2026-02-14 12:36:17 +0530

    Add nested if statements to compare three numbers

---
+++ conditional_statements/04_nestedif.py
@@ -0,0 +1,15 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=int(input("enter the number"))
+if(a>b):
+    if(a>c):
+        print("a is bigger")
+    else:
+        print("c is bigger")
+
+else:
+    if(b>c):
+        print("b is bigger")
+    else:
+        print("c is bigger")
+        
