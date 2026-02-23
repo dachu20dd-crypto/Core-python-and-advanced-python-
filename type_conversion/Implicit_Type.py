a80506e7afee672c6dc2793b77aa713aa27fa10f
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2026-02-23 20:55:46 +0530

    Add implicit type conversion example in Python

---
+++ type_conversion/Implicit_Type.py
@@ -0,0 +1,9 @@
+# Implicit Type Conversion Example
+
+num_int = 10        # integer
+num_float = 5.5     # float
+
+result = num_int + num_float  # Python converts int to float automatically
+
+print("Result:", result)
+print("Type of result:", type(result))
