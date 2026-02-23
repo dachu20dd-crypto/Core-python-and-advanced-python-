76fae1041aa2d4a7175c92e4932d508f3ddb5f28
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2026-02-23 08:57:56 +0530

    Add FizzBuzz function to check numbers up to n using elif

---
+++ conditional_statements/05_fizzbuzz.py
@@ -0,0 +1,11 @@
+def check_number(n):
+    for i in range(1,n+1):
+        if i%3==0 and i%5==0:
+            print(i,"fizzbuzz")
+        elif i%3==0:
+            print(i,"fizz")
+        elif i%5==0:
+            print(i,"buzz")
+        else:
+            print(i)
+check_number(20)
