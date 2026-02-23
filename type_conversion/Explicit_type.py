811ab44e609972cdaa80f74f3a49278de560448b
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2026-02-23 21:18:06 +0530

    Add explicit type conversion example in Python

---
+++ type_conversion/Explicit_type.py
@@ -0,0 +1,12 @@
+# Explicit Type Conversion Example
+
+num_str = "25"   # string
+
+# Convert string to integer
+num_int = int(num_str)
+
+result = num_int + 5
+
+print("Converted number:", num_int)
+print("Result:", result)
+print("Type after conversion:", type(num_int))
